4a3f159169a8d514fbe2d0103b6716e88fd55add
Dilshan Gunasekara <nilushadilshan209@gmail.com>
2026-01-03 00:21:54 +0530

    finished responsive style

--- navigation.js
+++ navigation.js
@@ -4,6 +4,7 @@ const shoppingCart = document.querySelector('.shopping-cart');
 const loginForm = document.querySelector('.login-form');
 const navbar = document.querySelector('.navbar');
 
+
 function closeAll() {
   searchForm?.classList.remove('active');
   shoppingCart?.classList.remove('active');
@@ -19,13 +20,6 @@ document.querySelector('#search-btn')?.addEventListener('click', () => {
   navbar?.classList.remove('active');
 });
 
-document.querySelector('#menu-btn')?.addEventListener('click', () => {
-  navbar?.classList.toggle('active');
-  searchForm?.classList.remove('active');
-  shoppingCart?.classList.remove('active');
-  loginForm?.classList.remove('active');
-});
-
 document.querySelector('#cart-btn')?.addEventListener('click', () => {
   shoppingCart?.classList.toggle('active');
   searchForm?.classList.remove('active');
@@ -33,6 +27,22 @@ document.querySelector('#cart-btn')?.addEventListener('click', () => {
   navbar?.classList.remove('active');
 });
 
+document.querySelector('#login-btn')?.addEventListener('click', () => {
+  loginForm?.classList.toggle('active');
+  searchForm?.classList.remove('active');
+  shoppingCart?.classList.remove('active');
+  navbar?.classList.remove('active');
+});
+
+document.querySelector('#menu-btn')?.addEventListener('click', () => {
+  navbar?.classList.toggle('active');
+  searchForm?.classList.remove('active');
+  shoppingCart?.classList.remove('active');
+  loginForm?.classList.remove('active');
+});
+
+// scrolling
+window.addEventListener('scroll', closeAll);
 
 
 

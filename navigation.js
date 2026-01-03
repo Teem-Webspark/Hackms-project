e8dca8436f5583a8d2390bab9a9510b2173bdd76
Dilshan Gunasekara <nilushadilshan209@gmail.com>
2026-01-03 05:35:29 +0530

    created more

--- navigation.js
+++ navigation.js
@@ -45,6 +45,39 @@ document.querySelector('#menu-btn')?.addEventListener('click', () => {
 window.addEventListener('scroll', closeAll);
 
 
+//swiper section
+
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    }
+  }
+
+  
+});
+
 
 
 
